81543288f79e8c2f1c1f7f9879488fc9b635fdc1
mseppi <miikka.seppi123@gmail.com>
2024-08-14 02:43:03 +0300

    fixed information

--- osa2/maiden_tiedot/src/App.jsx
+++ osa2/maiden_tiedot/src/App.jsx
@@ -19,8 +19,8 @@ const Country = ({ country }) => {
     <div>
       <h1>{country.name.common}</h1>
       <div>capital {country.capital[0]}</div>
-      <div>population {country.population}</div>
-      <h2>languages</h2>
+      <div>area {country.area}</div>
+      <h2>languages:</h2>
       <ul>
         {Object.values(country.languages).map(language => (
           <li key={language}>{language}</li>
