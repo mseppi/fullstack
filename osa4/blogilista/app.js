84b52af2c1fb63523234c7e6ccaca28e673fdbec
mseppi <miikka.seppi123@gmail.com>
2025-02-05 19:10:45 +0200

    do 4.1-4.2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+
+mongoose.connect(config.mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
