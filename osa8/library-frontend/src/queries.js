7b7e0beda708653f63caea2087e00bb1e30cb437
mseppi <miikka.seppi123@gmail.com>
2025-06-26 03:33:21 +0300

    do 8.8-8.12

---
+++ osa8/library-frontend/src/queries.js
@@ -0,0 +1,46 @@
+import { gql } from '@apollo/client';
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+      id
+    }
+  }
+`;
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      published
+      author
+      id
+      genres
+    }
+  }
+`;
+
+export const ADD_BOOK = gql`
+  mutation addBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+    addBook(title: $title, author: $author, published: $published, genres: $genres) {
+      title
+      published
+      author
+      id
+      genres
+    }
+  }
+`;
+
+export const EDIT_AUTHOR = gql`
+  mutation editAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      name
+      born
+      id
+    }
+  }
+`;
